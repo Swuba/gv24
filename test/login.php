deb0d6a4331ae122487cca9bd009c3ddfaa1b8f5
Filzknoetche <felixbickelmann@hotmail.de>
2017-12-04 17:39:45 +0100

    login & werbung test

---
+++ test/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- top -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-2802451501665411"
+     data-ad-slot="5726722253"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <form class="loginform" action="#" method="post">
+      <input type="text" name="username" placeholder="Benutzername"><br><br>
+      <input type="password" name="password" placeholder="Passwort"><br><br>
+      <input type="submit" name="submit" value="Anmelden">
+    </form>
+  </body>
+</html>
