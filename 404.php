913824367d2c5dd1f306ccd4d8147e9081d1e5bb
Filzknoetche <felixbickelmann@hotmail.de>
2017-12-13 21:34:25 +0100

    htaccess und 404fehlerseite

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+ include "includes/header.php";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>404</title>
+  </head>
+  <body>
+    <h1>Fehler</h1>
+  </body>
+</html>
