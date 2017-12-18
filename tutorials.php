aff82c100b9b735cfff6bebf737921e0039412ea
Filzknoetche <felixbickelmann@hotmail.de>
2017-12-18 22:33:06 +0100

    edit css -tutorialübersicht

--- tutorials.php
+++ tutorials.php
@@ -15,13 +15,14 @@
     <div class="row">
       <div class="col-xs-12">
         <h2>Wähle eine Kategorie</h2>
-        <h3 class="index-courses-subheader">Hier findest du alle möglichen Tutorials, wie z.B zu dem Raspberry Pi, Programmiersprachen, API nutzungen von z.B. League of Legend und Destiny 2 und vieles mehr...</h3>
+        <h3 class="index-courses-subheader">Hier findest du alle möglichen Tutorials, wie z.B zu dem Raspberry Pi,
+          Programmiersprachen, API nutzungen von z.B.von League of Legend und vieles mehr...</h3>
         <div class="row">
             <div class="col-xs-6">
                 <h2>Raspberry Pi</h2>
                 <br>
                 <p>
-                  Tutorials, wie du deinen Raspberry Pi einrichtest, einen Webserver einrichtests und vieles mehr!
+                  Tutorials, wie du deinen Raspberry Pi einrichtest, einen Webserver installierst und vieles mehr!
                  </p>
                 <p><a href="tutorials/raspberry-pi-tutorials.php" class="btn btn-success">zum Tutorial &raquo;</a></p>
             </div>
