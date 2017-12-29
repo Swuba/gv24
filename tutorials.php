0540303cb14324f6505fa8708aad1904dea40698
Filzknoetche <felixbickelmann@hotmail.de>
2017-12-29 19:57:34 +0100

    kategorieren geadded

--- tutorials.php
+++ tutorials.php
@@ -19,6 +19,7 @@
           Programmiersprachen, API nutzungen von z.B.von League of Legend und vieles mehr...</h3>
         <div class="row">
             <div class="col-xs-6">
+              <!-- Raspberry Pi -->
               <a href="tutorials/raspberry-pi-tutorials.php">
                 <div class="index-courses-outerbox courses-rp">
                   <div class="index-courses-icon">
@@ -34,8 +35,25 @@
                   </div>
                 </div>
               </a>
+              <!-- Hardware -->
+              <a href="tutorials/raspberry-pi-tutorials.php">
+                <div class="index-courses-outerbox courses-hw">
+                  <div class="index-courses-icon">
+                    <div>
+                      <img src="img/cpu.png" alt="">
+                    </div>
+                  </div>
+                  <div class="index-courses-box">
+                    <div>
+                      <h3>Hardware</h3>
+                      <p>Tutorials, die dir Zeigen, wie du einen Raspberry Pi 3 anschließt und einrichtest, einen Webserver installierst und vieles mehr!</p>
+                    </div>
+                  </div>
+                </div>
+              </a>
             </div>
             <div class="col-xs-6">
+              <!-- Programmieren -->
               <a href="tutorials/programmier-tutorials.php">
                 <div class="index-courses-outerbox courses-coding">
                   <div class="index-courses-icon">
@@ -51,6 +69,7 @@
                   </div>
                 </div>
               </a>
+              
             </div>
         </div>
       </div>
