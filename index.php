c01afed1e10ffca6dab1375fcc61c615097d01fc
Felix <felixbickelmann@gmail.com>
2017-12-30 19:35:23 +0100

    changed index

--- index.php
+++ index.php
@@ -41,6 +41,54 @@
 
   </div>
 </article>
+<article class="index-courses">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h2>Empfehlenswerte Tutorials</h2>
+        <h3 class="index-courses-subheader"></h3>
+        <div class="row">
+            <div class="col-xs-6">
+              <!-- Getting started -->
+              <a href="getting-started.php">
+                <div class="index-courses-outerbox courses-gs">
+                  <div class="index-courses-icon">
+                    <div>
+                      <img src="img/startup-white.png" alt="">
+                    </div>
+                  </div>
+                  <div class="index-courses-box">
+                    <div>
+                      <h3>Getting started</h3>
+                      <p>Tutorials, die dir Zeigen, wie du einen Raspberry Pi 3 anschließt und einrichtest, einen Webserver installierst und vieles mehr!</p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-6">
+              <!-- Webserver erstellen -->
+              <a href="raspberry-pi-webserver-erstellen.php">
+                <div class="index-courses-outerbox courses-webserver">
+                  <div class="index-courses-icon">
+                    <div>
+                      <img src="img/www-white.png" alt="">
+                    </div>
+                  </div>
+                  <div class="index-courses-box">
+                    <div>
+                      <h3>Webserver erstellen</h3>
+                      <p>Basics, wie du deine Entwicklungsumgebung einrichtest, der Start deiner eigenen Website, API´s verwendest z.B. von League of Legends und vieles mehr!</p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</article>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- tut1 -->
 <ins class="adsbygoogle"
